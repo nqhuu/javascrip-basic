aa553f688d3729d197c4fabf9b3c7737b197e63d
nqhuu <nqhuund@gmail.com>
2024-04-18 09:00:29 +0700

    bài 7+8

--- 2-Toan-tu/bai-7.js
+++ 2-Toan-tu/bai-7.js
@@ -1,1 +1,5 @@
 // Bài 7: Viết chương trình nhập vào giá trị cho a và b. Tính tổng a và b, nếu tổng nhỏ hơn 4, hiển thị chuỗi Below, ngược lại hiển thị Over. Lưu ý sử dụng toán tử ?
+let a = +prompt('nhập a');
+let b = +prompt('nhập b');
+let ketQua = ((a + b) < 4) ? 'Below' : 'Over';
+alert(ketQua);
--- 2-Toan-tu/bai-8.js
+++ 2-Toan-tu/bai-8.js
@@ -3,3 +3,7 @@
 // a.	Nhỏ hơn 20 độ C thông báo tăng nhiệt độ.
 // b.	Lớn hơn 100 độ C thông báo giảm nhiệt độ
 // c.	Còn lại nhiệt độ đủ dùng.
+
+let input = +prompt("Nhập nhiệt đô (độ C)");
+let output = (input <= 20) ? "tăng nhiệt độ" : (input >= 100) ? "Giảm nhiệt độ" : "Nhiệt độ đủ dùng"
+alert(output);
