2072bb13d53d2a58e295600655360bb9c6c8e59e
nqhuu <nqhuund@gmail.com>
2024-04-18 14:36:11 +0700

    bài 9+10

---
+++ 2-Toan-tu/bai-9.js
@@ -0,0 +1,4 @@
+// Bài 9: cho 
+let array = [3, 5, 4, 9, 8, 1, 10]
+// a.	hãy tính tổng của phần tử đầu và phần tử cuối // 3 + 10
+// b.	hãy thêm số 20 vào cuối array trên và tính lại tổng của phần tử đầu và phần tử cuối  // 3 + 20
