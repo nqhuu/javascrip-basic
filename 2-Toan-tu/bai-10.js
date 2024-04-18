3e2080912551fc9e8e00a90448b289c26bcfc066
nqhuu <nqhuund@gmail.com>
2024-04-18 14:47:36 +0700

    bài 9 + 10

--- 2-Toan-tu/bai-10.js
+++ 2-Toan-tu/bai-10.js
@@ -1,4 +1,5 @@
 // Bài 10:
+let input = prompt('Nhập pss');
 // prompt nhập password là 1 string bất kỳ
 // * Nếu password để trống thì in ra ‘password không để trống’
 // * Nếu password nhỏ hơn 8 ký tự hoặc password dài hơn 16 ký tự thì in ra ‘password k hợp lệ’
