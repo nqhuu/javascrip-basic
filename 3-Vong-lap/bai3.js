793c92b6923d0273904bb98095db8499ad7f849f
trungdai28 <trungdai28@gmail.com>
2024-04-22 22:34:11 +0700

    update

--- 3-Vong-lap/bai3.js
+++ 3-Vong-lap/bai3.js
@@ -4,14 +4,14 @@ data = [3,2,4,'5',1,8,7,'9']
 let num = [3, 2, 4, '5', 1, 8, 7, '9'];
 let sum = 0;
 for (let i = 0; i < num.length; i++) {
-    if (typeof num[i] === 'number') {
-        sum += num[i];
-    }
+    // if (typeof num[i] === 'number') {
+        sum += parseInt(num[i]);
+    // }
 }
 console.log(sum);
 
 // b.	hãy tạo ra array2 chứa các phần tử có giá trị lẻ của data
-let array2 = data.filter(item => typeof item === 'number' & item %2 !== 0);
+let array2 = data.filter(item =>  item %2 !== 0);
 console.log(array2);
 
 // c.	hãy tạo ra array3 chứa các phần tử của data theo thứ tự  đảo ngược 
